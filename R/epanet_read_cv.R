6d56e2285405be8bfd41b2c48ddbb5deabbf0265
ratral <dr.ing.trujillo@gmail.com>
2022-07-29 14:32:41 +0200

    Function controlvalves_in_epanet.Rmd

--- R/epanet_read_cv.R
+++ R/epanet_read_cv.R
@@ -11,34 +11,42 @@
 #' @return The table operation_data
 #' @export
 #'
+#' @import dplyr
+#' @import tidyr
+#' @import tibble
+#'
+#'
 epanet_read_cv <- function(hmodel, hreport, cv_name) {
+
+  ID <- Node1 <- Node2 <- Diameter <- Type <- MinorLoss <- NULL
+  Pressure <- Timestamp <- Headloss <- Flow <- Velocity <- p1 <- p2 <- NULL
 
   # Read  from the hmodel the parameter of the control valve to evaluate.
   cvalve <- hmodel$Valves |> filter(ID == cv_name) |>
-    select(ID, Node1, Node2, Diameter, Type, MinorLoss)
+    dplyr::select(ID, Node1, Node2, Diameter, Type, MinorLoss)
 
   # Values for the Nodes of the Valve
   nodes <- hreport$nodeResults |>
-    filter(ID %in% c(cvalve$Node1[1], cvalve$Node2[1])) |>
-    select(ID, Pressure, Timestamp) |>
-      pivot_wider(names_from = ID, values_from = Pressure)
+    dplyr::filter(ID %in% c(cvalve$Node1[1], cvalve$Node2[1])) |>
+    dplyr::select(ID, Pressure, Timestamp) |>
+    tidyr::pivot_wider(names_from = ID, values_from = Pressure)
 
   names(nodes) <- c("Timestamp", "p1", "p2")
 
   # Values for the Valve (Link))
-  links <- as_tibble( hreport$linkResults) |>
-    filter(ID == cv_name) |>
-    select(Timestamp, Headloss, Flow, Velocity)
+  links <- tibble::as_tibble( hreport$linkResults) |>
+    dplyr::filter(ID == cv_name) |>
+    dplyr::select(Timestamp, Headloss, Flow, Velocity)
 
   operation_data <- nodes |>
-    full_join(links, by ="Timestamp") |>
-    mutate(
+    dplyr::full_join(links, by ="Timestamp") |>
+    dplyr::mutate(
       ID = cvalve$ID,
       Diameter = cvalve$Diameter,
       Type = cvalve$Type,
       MinorLoss = cvalve$MinorLoss
     ) |>
-    select(Timestamp,
+    dplyr::select(Timestamp,
            ID, Diameter, Type, MinorLoss,
            p1, p2, Headloss, Flow, Velocity)
 
