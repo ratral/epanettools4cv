76f5ccdd06653a94a65cdf5710307e5808a1a455
ratral <dr.ing.trujillo@gmail.com>
2022-08-05 13:43:02 +0200

    dara-raw development: Samples of Epanet

--- R/epanet_read_cv.R
+++ R/epanet_read_cv.R
@@ -22,11 +22,13 @@ epanet_read_cv <- function(hmodel, hreport, cv_name) {
   Pressure <- Timestamp <- Headloss <- Flow <- Velocity <- p1 <- p2 <- NULL
 
   # Read  from the hmodel the parameter of the control valve to evaluate.
-  cvalve <- hmodel$Valves |> filter(ID == cv_name) |>
+
+  cvalve <- tibble::as_tibble(hmodel$Valves) |>
+    dplyr::filter(ID == cv_name) |>
     dplyr::select(ID, Node1, Node2, Diameter, Type, MinorLoss)
 
   # Values for the Nodes of the Valve
-  nodes <- hreport$nodeResults |>
+  nodes <- tibble::as_tibble(hreport$nodeResults) |>
     dplyr::filter(ID %in% c(cvalve$Node1[1], cvalve$Node2[1])) |>
     dplyr::select(ID, Pressure, Timestamp) |>
     tidyr::pivot_wider(names_from = ID, values_from = Pressure) |>
@@ -37,7 +39,7 @@ epanet_read_cv <- function(hmodel, hreport, cv_name) {
   names(nodes) <- c("Timestamp", "p1", "p2")
 
   # Values for the Valve (Link))
-  links <- tibble::as_tibble( hreport$linkResults) |>
+  links <- tibble::as_tibble(hreport$linkResults) |>
     dplyr::filter(ID == cv_name) |>
     dplyr::select(Timestamp, Headloss, Flow, Velocity)
 
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,30 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `DATASET` data-set goes here
 
 library(dplyr)
 library(tibble)
 library(epanetReader)
+libray(here)
+
+# tibble with the different network models
+network_models <- tibble::tibble(
+  ntw.name = c(
+    "Res2Res_001",
+    "basicDMA_001",
+    "basicDMA_002",
+    "basicDMA_003",
+    "basicDMA_004"
+  ),
+  inp.file = as.list(NA),
+  rpt.file = as.list(NA)
+)
+
+network_models$inp.file <- purrr::map(
+    here::here("data-raw", "epanet", paste0(network_models$ntw.name, ".inp")),
+    ~read.inp(.x)
+  )
 
 
 
 
-
-# usethis::use_data(DATASET, overwrite = TRUE)
+# usethis::use_data(network_models, overwrite = TRUE)
 
